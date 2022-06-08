82601df53d24ae5230b080388b157327dcd86a4b
Florian Becher <florian.becher@quibiq.ch>
2022-06-08 18:59:59 +0200

    Change of message

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 app.use(express.json());
 
-// Get and send JSON
 const port = process.env.PORT || 3000;
 
-
 app.get('/', (req, res) => {
-    res.end('Working');
+    res.end('200: Working API Response for Testing');
 });
 
 app.listen(port, () => {
